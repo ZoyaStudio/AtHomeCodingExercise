0e9544ccf41de5f71eb9fad2fbb5e18620c175da
Elizabeth Mendenhall <eamendenhall@outlook.com>
2021-10-21 22:46:33 -0700

    Implemented aboveBelow.

--- code.js
+++ code.js
@@ -3,6 +3,18 @@ class takeHome {
 
   constructor () {}
 
+  aboveBelow (collection, integer) {
+    let result = {"above": 0, "below": 0};
+    for (let num of collection) {
+      if (num > integer) {
+        result.above++;
+      } else if (num < integer) {
+        result.below++;
+      }
+    }
+    return result;
+   }
+
   stringRotation (string, index) {
    return string.slice(string.length - index) + string.slice(0, string.length - index)
   }
